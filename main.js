3d906daee88ddb2232da5a7ec86d4e25403a049e
Saba <sabanafchigar@gmail.com>
2023-01-28 16:26:20 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let x = +prompt("x..."); // for example : 10
+let y = +prompt("y..."); // for example : 3
+x+= y;
+console.log(x); //13
+x-=y
+console.log(x); //10
+x*=y
+console.log(x); //30
+x/=y
+console.log(x); //10
+x%=y
+console.log(x); //1
