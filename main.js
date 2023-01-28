8e6db7ae63f7d18f18ddc83ab048e910906c46e8
Saba <sabanafchigar@gmail.com>
2023-01-28 17:38:47 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let x = +prompt("x..."); // for example : 5
+let y = +prompt("y..."); // for example : 6
+
+console.log(x == y); // false
+console.log(x === y); // false ... first check type then value
+console.log(x != y); // true
+console.log(x !== y); // true
+console.log(x > y); // false
+console.log(x < y); // true
+console.log(x >= y); // false
+console.log(x <= y); // true
+
