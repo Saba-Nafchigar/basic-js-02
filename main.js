520d5f84dd90a5899f0ca68d3a3d41fe1e0a9b3b
Saba <sabanafchigar@gmail.com>
2023-01-29 00:16:57 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let a = +prompt("a...")
+let b = +prompt("b...")
+let result= a > b &&  a === 10 && b === 5
+console.log(result);
+
+
+
+
+let x = +prompt("x..."); // for example : 5
+let y = +prompt("y..."); // for example : 6
+
+if (x > 10 && y > 10 ){
+    console.log("x and y is bigger than 10");
+}
+else if(x > 10 || y > 10){
+ console.log("x or y is bigger than 10");
+}
+else if(x!=y){
+    console.log("x is not equal to y!");
+}
+
